585366d6f6d0b46fb1d9bb9c786c880ec7434ff7
Syncz <raisyadjullfikar@gmail.com>
2025-09-22 13:56:51 +0700

    1. Two Sum

---
+++ 1. Two Sum.php
@@ -0,0 +1,15 @@
+class Solution {
+    
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        for ($countA = 0; $countA < count($nums); $countA++){
+            for ($countB = $countA + 1; $countB < count($nums); $countB++){
+                if ($nums[$countA] + $nums[$countB] == $target) return [$countA, $countB];
+            }
+        }
+    }
+}
