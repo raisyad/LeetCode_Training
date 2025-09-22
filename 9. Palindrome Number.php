0d1f65bcd9ad7f188eb59c77d73de62ffb519a4e
Syncz <raisyadjullfikar@gmail.com>
2025-09-22 13:58:15 +0700

    9. Palindrome Number

---
+++ 9. Palindrome Number.php
@@ -0,0 +1,17 @@
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Boolean
+     */
+    function isPalindrome($x) {
+        $num = (string)$x;
+        $len = strlen($num);
+
+        for ($i = 0; $i < $len/2; $i++) {
+            if ($num[$i] == $num[$len - $i - 1]) continue;
+            else return false;
+        }
+        return true;
+    }
+}
