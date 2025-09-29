77e0939f6b1077a28f451ed87febb16305e5d158
Syncz <raisyadjullfikar@gmail.com>
2025-09-29 13:21:10 +0700

    136. Single Number

---
+++ 136. Single Number.php
@@ -0,0 +1,22 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function singleNumber($nums) {
+        $len = count($nums);
+        $check = [];
+        if ($len <= 1 && $nums[0] > 0) return $len;
+
+        for ($i = 0; $i < $len; $i++) {
+            $x = $nums[$i];
+            $check[$x] = ($check[$x] ?? 0) + 1;
+        }
+        asort($check);
+
+        foreach ($check as $angka => $jumlah) {
+            return $angka;
+        }
+    }
+}
