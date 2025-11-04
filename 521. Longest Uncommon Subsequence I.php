3b8a9f7dbeacfc63b4e4fd6279179127fd9ff7b4
Syncz <raisyadjullfikar@gmail.com>
2025-11-04 14:29:09 +0700

    521. Longest Uncommon Subsequence I

---
+++ 521. Longest Uncommon Subsequence I.php
@@ -0,0 +1,13 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $a
+     * @param String $b
+     * @return Integer
+     */
+    function findLUSlength($a, $b) {
+        return $a === $b ? -1 : max(strlen($a), strlen($b));
+    }
+}
