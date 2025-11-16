771edee9a0412b71e47cad91d8635fde60aeee8f
Syncz <raisyadjullfikar@gmail.com>
2025-11-16 23:07:17 +0700

    27. Remove Element.php

---
+++ 27. Remove Element.php
@@ -0,0 +1,19 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $val
+     * @return Integer
+     */
+    function removeElement(&$nums, $val) {
+        $k = 0;
+        for ($i = 0; $i < count($nums); $i++) {
+            if ($nums[$i] != $val) {
+                $nums[$k] = $nums[$i]; $k++;
+            }
+        }
+        return $k;
+    }
+}
